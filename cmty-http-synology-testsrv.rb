4c32505e63726d04c5aed77791713dacff184035
BrianWGray <BrianWGray@gmail.com>
2014-08-28 17:41:54 -0400

    Re-implemented the server script to better present a valid http response.

--- cmty-http-synology-testsrv.rb
+++ cmty-http-synology-testsrv.rb
@@ -2,32 +2,31 @@ require 'socket'
 require 'cgi'
 
 
-htmlReply = "<html> <script type='text/javascript'>parent.location = '#major=4&minor=2&build=3211&junior=0&unique=synology_cedarview_rs2212+&sn=D4LFN00210'</script> </html>"
+
+server = TCPServer.new 5000
+puts "Test server listening\r\n\r\n"
+
+response = "<html> <script type='text/javascript'>parent.location = '#major=4&minor=2&build=3211&junior=0&unique=synology_cedarview_rs2212+&sn=D4LFN00210'</script> </html>"
 
 
-server = TCPServer.open 5000
-puts "Test server listening on port 5000\r\n\r\n"
-
-loop {
+loop do
+    
     client = server.accept
     
-    lines = []
-    while line = client.gets and line !~ /^\s*$/
-        lines << line.chomp
-    end
+    request = client.gets
+    
+    STDERR.puts request
     
     time = CGI.rfc1123_date(Time.now)
     
-    resp = lines.join("<br />")
     headers = ["http/1.1 200 ok",
-    time,
+    "#{time}",
     "Server: Apache/2.2.23 (Unix) mod_ssl/2.2.23 OpenSSL/1.0.1e-fips",
     "Vary: Accept-Encoding",
-    "content-type: text/html; charset=iso-8859-1",
-    "content-length: #{resp.length}\r\n\r\n"].join("\r\n")
-    client.puts htmlReply
+    "content-type: text/html; charset=\"UTF-8\"",
+    "content-length: #{response.bytesize}",
+    "Connection: close\r\n\r\n"].join("\r\n")
+    client.puts headers
+    client.puts response
     client.close
-    puts headers
-    puts "Request: #{resp} \r\n\r\n"
-    puts "Reply: #{htmlReply} \r\n\r\n"
-}
+end
