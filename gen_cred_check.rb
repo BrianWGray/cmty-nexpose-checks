961f2df74676bb9761abd2e1af4ec0fa2ea8e991
BrianWGray <BrianWGray@gmail.com>
2016-10-21 12:39:23 -0400

    add http auth check support

--- gen_cred_check.rb
+++ gen_cred_check.rb
@@ -18,7 +18,7 @@ checkType = "ssh" # For now force service type to SSH
 
 ## Collect interactive information
 # TODO: make this an optional collection type and also provide various arg entry options.
-puts "Enter Check Type: (SSH, Telnet)"
+puts "Enter Check Type: (SSH, Telnet, HTTP)"
 checkType = gets.chomp
 
 puts "Enter user account:"
@@ -59,6 +59,16 @@ def vck_file_content(username, password, checkType)
         vckFileContent += "       </DefaultAccount>\n"
         vckFileContent += "</VulnerabilityCheck>\n"
 
+      when "http"
+
+        vckFileContent =  "<VulnerabilityCheck id=\"cmty-#{@checkType.downcase}-default-account-#{@username}-password-#{@password}\" scope=\"endpoint\">\n"
+        vckFileContent += "    <NetworkService type=\"HTTP|HTTPS\"/>\n"
+        vckFileContent += "       <DefaultAccount>\n"
+        vckFileContent += "          <uid>#{@username}</uid>\n"
+        vckFileContent += "          <password><![CDATA[#{@password}]]></password>\n"
+        vckFileContent += "       </DefaultAccount>\n"
+        vckFileContent += "</VulnerabilityCheck>\n"
+      
       else
         raise Invalid, "service type unrecognized"
   end
@@ -114,6 +124,35 @@ def xml_file_content(username, password, checkType, timeInfo)
       xmlFileContent += "    <p>The #{@username} account uses a password of &quot;<![CDATA[#{@password}]]>&quot;.  This would allow\n"
       xmlFileContent += "      anyone to log into the machine via #{@checkType.upcase} and take complete\n"
       xmlFileContent += "      control.</p>\n"
+      xmlFileContent += "  </Description>\n"
+      xmlFileContent += "  <Solutions>\n"
+      xmlFileContent += "    <Solution id=\"cmty-#{@checkType}-default-account-#{@username}-password-#{@password}\" time=\"15m\">\n"
+      xmlFileContent += "      <summary>Fix Default #{@checkType.upcase} account: #{@username} password: <![CDATA[#{@password}]]></summary>\n"
+      xmlFileContent += "      <workaround>\n"
+      xmlFileContent += "        <p>\n"
+      xmlFileContent += "          Change the password to a strong non-default value.\n"
+      xmlFileContent += "        </p>\n"
+      xmlFileContent += "      </workaround>\n"
+      xmlFileContent += "    </Solution>\n"
+      xmlFileContent += "  </Solutions>\n"
+      xmlFileContent += "</Vulnerability>\n"
+
+    when "http"
+      xmlFileContent = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+      xmlFileContent += "<Vulnerability id=\"cmty-#{@checkType.downcase}-default-account-#{@username}-password-#{@password}\" published=\"#{@timeInfo.strftime("%Y-%m-%d")}\" added=\"#{@timeInfo.strftime("%Y-%m-%d")}\" modified=\"#{@timeInfo.strftime("%Y-%m-%d")}\" version=\"1.0\">\n"
+      xmlFileContent += "  <name>Default #{@checkType} account: #{@username} password \"<![CDATA[#{@password}]]>\"</name>\n"
+      xmlFileContent += "  <severity>8</severity>\n"
+      xmlFileContent += "  <cvss>(AV:N/AC:L/Au:S/C:P/I:C/A:P)</cvss>\n"
+      xmlFileContent += "  <Tags>\n"
+      xmlFileContent += "    <tag>Default Account</tag>\n"
+      xmlFileContent += "    <tag>#{@checkType.upcase}</tag>\n"
+      xmlFileContent += "  </Tags>\n"
+      xmlFileContent += "  <AlternateIds>\n"
+      xmlFileContent += "  </AlternateIds>\n\n"
+      xmlFileContent += "  <Description>\n"
+      xmlFileContent += "    <p>The #{@username} account uses a password of &quot;<![CDATA[#{@password}]]>&quot;.  This would allow\n"
+      xmlFileContent += "      anyone to log into the web application via #{@checkType.upcase} and abuse the application\n"
+      xmlFileContent += "      </p>\n"
       xmlFileContent += "  </Description>\n"
       xmlFileContent += "  <Solutions>\n"
       xmlFileContent += "    <Solution id=\"cmty-#{@checkType}-default-account-#{@username}-password-#{@password}\" time=\"15m\">\n"
