2c48bfd9ddd6041a29c3d91443fe701bd00e680f
BrianWGray <BrianWGray@gmail.com>
2018-03-02 15:20:45 -0500

    simplified solutions for restricting access to memcached

---
+++ memcached-restrict.sol
@@ -0,0 +1,13 @@
+<Solution id="memcached-restrict" time="2h">
+  <summary>Restrict access to memcached service</summary>
+  <workaround>
+  <p>Consider performing one or more of the following actions in order to mitigate abuse
+of this service:</p>
+<ol>
+    <li> Restrict access to the memcached service to only trusted assets. </li>
+    <li> Consider whether the UDP implementation of memcached is necessary. </li></ol>
+  </workaround>
+  <AdditionalInfo>
+    <p>If the memcached service is not needed, consider disabling or uninstalling the service entirely. </p>
+  </AdditionalInfo>
+</Solution>
