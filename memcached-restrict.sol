7b8d3ce78db232556b79e7d4b6391fd4e5a1545f
BrianWGray <BrianWGray@gmail.com>
2018-03-05 17:53:24 -0500

    content added

--- memcached-restrict.sol
+++ memcached-restrict.sol
@@ -8,6 +8,7 @@ of this service:</p>
     <li> Consider whether the UDP implementation of memcached is necessary. </li></ol>
   </workaround>
   <AdditionalInfo>
+    <p> On memcached startup you can specify <pre>--listen 127.0.0.1</pre> to listen only to localhost and <pre>-U 0</pre> to disable UDP completely. By default memcached listens on INADDR_ANY and runs with UDP support ENABLED.</p>
     <p>If the memcached service is not needed, consider disabling or uninstalling the service entirely. </p>
   </AdditionalInfo>
 </Solution>
