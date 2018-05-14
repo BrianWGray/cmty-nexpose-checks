ab0cf2886d1ced1414b3f5091475b59e7744426c
BrianWGray <BrianWGray@gmail.com>
2018-05-14 14:57:44 -0400

    vulnerability check to detect CLDAP amplification - BrianWGray

---
+++ cmty-cldap-restrict.sol
@@ -0,0 +1,12 @@
+<Solution id="cmty-cldap-restrict" time="2h">
+  <summary>Restrict access to the CLDAP Service</summary>
+  <workaround>
+  <p>Consider performing one or more of the following actions in order to mitigate abuse
+of this service:</p>
+<ol>
+    <li> Restrict access to the CLDAP service to only trusted assets. </li></ol>
+  </workaround>
+  <AdditionalInfo>
+    <p>If the CLDAP service is not needed, consider disabling or uninstalling the service entirely. </p>
+  </AdditionalInfo>
+</Solution>
