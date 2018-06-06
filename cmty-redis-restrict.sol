30cb0bf016f843858032909133074cc8ad624db0
BrianWGray <BrianWGray@gmail.com>
2018-06-06 14:13:12 -0400

    initial commit

---
+++ cmty-redis-restrict.sol
@@ -0,0 +1,14 @@
+<Solution id="cmty-redis-remotecommand" time="2h">
+  <summary>Restrict access to Redis service</summary>
+  <workaround>
+  <p>Consider performing one or more of the following actions in order to mitigate abuse
+of this service:</p>
+  <ol>
+    <li> Restrict access to the Redis service to only trusted assets. </li>
+  </ol>
+  </workaround>
+  <AdditionalInfo>
+    <p>Configure protected mode for the service as a minimum mitigation.</p>
+    <p>If the Redis service is not needed, consider disabling or uninstalling the service entirely. </p>
+  </AdditionalInfo>
+</Solution>
