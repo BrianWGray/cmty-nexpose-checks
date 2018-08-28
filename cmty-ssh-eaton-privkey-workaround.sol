2fdeb91d848c8850abd84a12c3d0acd1724f2a35
BrianWGray <BrianWGray@gmail.com>
2018-08-28 17:24:11 -0400

    Eaton Power Xpert Meter PoC

---
+++ cmty-ssh-eaton-privkey-workaround.sol
@@ -0,0 +1,7 @@
+<?xml version='1.0' encoding='UTF-8'?>
+<Solution id="cmty-ssh-eaton-privkey-workaround" time="1h">
+  <summary>Upgrade to a non-vulnerable release or disable SSH access</summary>
+  <workaround>
+    <p>A new version of this firmware already exists for meters with firmware versions 13.x and above. Eaton recommends all customers using the products above to install the newest firmware. Please download the latest firmware (Version 13.4.0.10) from the product portal. In the future, as new firmware is made available for the power Xpert Meter 4000/6000/8000, Eaton recommends that customers upgrade to the latest available release of firmware. Power Xpert Meters 4000/6000/8000 with firmware version 12 and below were manufactured before September 2010 and are considered an end-of-life hardware platform. These older meters should be replaced with new Power Xpert Meters 4000/6000/8000.</p>
+  </workaround>
+</Solution>
